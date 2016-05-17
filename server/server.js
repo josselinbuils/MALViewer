2cad804ca835a99e00ebd922177a78563eec98e8
Josselin BUILS <josselin.buils@gmail.com>
2016-05-17 22:42:18 +0200

    Serve images

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 
 const Logger = require('./logger');
 
@@ -10,6 +11,44 @@ const port = process.env.PORT;
 if (!port) {
     throw Error('Port should be provided as environment variable')
 }
+
+app.get('/get/:url', (req, res) => {
+
+    let url = decodeURIComponent(req.params.url).match(/(https?:\/\/[^/]*)(.*)/);
+
+    let reqConfig = {
+        hostname: url[1],
+        path: url[2],
+        method: 'GET'
+    };
+
+    let request = http.request(reqConfig, function (res) {
+
+        let buffer = new Buffer();
+
+        if (res.statusCode === 200) {
+
+            res.writeHead(200, {'Content-Type': res.getHeader('Content-Type')});
+
+            res.on('data', function (data) {
+                buffer.write(data, 'binary');
+            });
+
+            res.on('end', function () {
+                res.end(buffer);
+            });
+
+        } else {
+            res.status(res.statusCode).end(res.statusMessage);
+        }
+    });
+
+    request.on('error', function () {
+        res.status(500).end();
+    });
+
+    request.end();
+});
 
 app.use(express.static(process.cwd() + '/client/dist'));
 
