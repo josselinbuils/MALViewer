40b0bdae145c5af81a05a315e1730a9b08770f14
Josselin BUILS <josselin.buils@gmail.com>
2016-05-18 19:03:58 +0200

    Moved the proxy inside ReverseProxy

--- server/server.js
+++ server/server.js
@@ -13,24 +13,7 @@ if (!port) {
     throw Error('Port should be provided as environment variable')
 }
 
-let proxy = httpProxy.createProxyServer({});
-proxy.on('error', error => Logger.error('Proxy error: ' + error.message));
-
-app.get('/get/:url', (req, res) => {
-
-    let url = decodeURIComponent(req.params.url);
-
-    Logger.info('GET ' + url);
-
-    proxy.web(req, res, {
-        changeOrigin: true,
-        ignorePath: true,
-        target: url
-    });
-});
-
 app.use(express.static(process.cwd() + '/client/dist'));
-
 app.listen(port);
 
 Logger.info('MALViewer is listening on port ' + port);
